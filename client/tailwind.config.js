7618e7fdc179c1a54e40d64b16637fce41169166
sadiquex <abubakasaddik1@gmail.com>
2025-08-25 07:58:07 +0000

    feat: update tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+        },
+      },
+    },
   },
   plugins: [],
 };
